83c99d348b751caa0b4dd2133739911c9ad51ad3
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-31 05:17:37 +0600

    Hero Register section added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Register = () => {
+      const handleRegister = (e) => {
+            e.preventDefault();
+            const email = e.target.email.value;
+            const password = e.target.password.value;
+            console.log("email : ", email, "password : ", password);
+      }
       return (
-            <div>
-                  <h3 className="text-3xl">Please Register here</h3>
+            <div className=''>
+                  <div className='mx-auto md:w-1/2'>
+                        <h3 className="text-3xl py-4">Please Register here</h3>
+                        <form onSubmit={handleRegister}>
+                              <input className='mb-4 w-3/4 p-3' type="email" name='email' id='' placeholder='Email Address'/>
+                              <br />
+                              <input className='mb-4 w-3/4 p-3' type="password" name='password' id='' placeholder='Password'/>
+                              <br />
+                              <input className='btn btn-secondary mb-4 w-3/4' type="submit" value="Register" />
+                        </form>
+                  </div>
             </div>
       );
 };
