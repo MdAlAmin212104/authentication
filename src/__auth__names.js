ca8b4bd40c741c7903b5caf6b9abec18b3dc4339
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-31 04:44:03 +0600

    first commit

---
+++ src/__auth__names.js
@@ -0,0 +1,7 @@
+/******
+ * 1. Authentication, authorization
+ * 2. login === sing in
+ * 3. register === sing up
+ * 
+ * 
+ */
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+      return (
+            <div>
+                  <h3 className="text-3xl">Please Register here</h3>
+            </div>
+      );
+};
+
+export default Register;
